dd1d1e2a31c1894aa050ee07e1066ea3d1942cbf
quangdaica <anhdhqse05546@fpt.edu.vn>
2018-09-08 03:19:59 +0700

    sua

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,23 @@ import React, { Component } from 'react';
 import './App.css'
 import Login from './Container/Login'
 import Books from './Container/Books'
+import { Route, Switch, BrowserRouter } from 'react-router-dom'
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Books />
+        <BrowserRouter>
+          <Switch>
+            <Route exact path='/' render={(props) => {
+              return <Books />
+            }} />
+            <Route path='/xxx' render={(props) => {
+              return <Login />
+            }} />
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
